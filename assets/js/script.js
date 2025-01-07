aa55229b3c1e6fe4b60b66ca08d3ecb3cc532ca0
Lizzy van <lizzyvanrhijn@hotmail.com>
2025-01-07 13:31:57 +0000

    Add function to populate column 2 with random fruits each time you click GO

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,39 @@ const cherries = '&#127826;';
 const kiwi = '&#129373;';
 
 // Array to store fruits
-
 const fruitArray = [grapes, lemon, pineapple, cherries, kiwi];
 
-// Code to populate second fruit column
 
-document.getElementById('second-column-top').innerHTML= kiwi;
+// Click event added to button, calls function selectFruitFunction
+document.getElementById('button-go').addEventListener('click', selectFruitFunction);
 
- 
+// Introduce empty array for second fruit column, changable variable
+let fruitColumnTwo = [];
+
+// resetArray function to empty array
+function resetArray(){
+    fruitColumnTwo = [];
+
+}
+
+// selectFruitFunction called by GO button
+function selectFruitFunction() {
+
+    // Call resetFunction to empty array
+    resetArray();
+
+// Loop through main fruitArray to get 3 random fruits and push them to empty fruitColumnTwo
+
+for (let i=0; i < 3; i++){
+    let randomFruit = fruitArray[Math.floor((Math.random()*fruitArray.length))];
+
+    fruitColumnTwo.push(randomFruit);
+
+   }
+
+// Populate second fruit column with three random fruits from fruitColumnTwo array
+
+document.getElementById('second-column-top').innerHTML = fruitColumnTwo[0];
+document.getElementById('second-column-middle').innerHTML = fruitColumnTwo[1];
+document.getElementById('second-column-bottom').innerHTML = fruitColumnTwo[2];
+}
