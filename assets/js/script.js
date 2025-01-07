1481532a001da2a78b7bbdbc657afe3d38d7c4ae
Lizzy van <lizzyvanrhijn@hotmail.com>
2025-01-07 12:47:23 +0000

    Populate html with fruits via js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Constants for fruits
+
+const grapes = '&#127815;';
+const lemon = '&#127819;';
+const pineapple = '&#127821;';
+const cherries = '&#127826;';
+const kiwi = '&#129373;';
+
+// Array to store fruits
+
+const fruitArray = [grapes, lemon, pineapple, cherries, kiwi];
+
+// Code to populate second fruit column
+
+document.getElementById('second-column-top').innerHTML= kiwi;
+
+ 
